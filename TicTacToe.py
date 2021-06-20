0f075acbf9c13362ad99e1e5cb58a1ea6532eeab
bsspencer42 <bsspencer42@gmail.com>
2021-06-20 16:18:23 -0500

    Create box class

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,2 +1,14 @@
 #Tic Tac Toe - Init
-print('Hello Paige')
+#Create box class
+class box:
+    def __init__(self,length = 1, width = 1, height = 1):
+        self.length = length
+        self.width = width
+        self.height = height
+    #Method to calculate volume
+    def get_volume(self):
+        return self.length * self.width * self.height
+
+#Begin Program
+newBox = box(height = 5)
+print(newBox.length)
