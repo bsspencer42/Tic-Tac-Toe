a0a7bf58af9f01ec3085e06d2c229b002de6af35
bsspencer42 <bsspencer42@gmail.com>
2021-06-20 16:30:39 -0500

    Create sphere class

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
 #Tic Tac Toe - Init
+import math
 #Create box class
 class box:
     def __init__(self,length = 1, width = 1, height = 1):
@@ -8,7 +9,14 @@ class box:
     #Method to calculate volume
     def get_volume(self):
         return self.length * self.width * self.height
+class sphere:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_volume(self):
+        return round((4/3)*math.pi**3,2)
 
 #Begin Program
 newBox = box(height = 5)
-print(newBox.length)
+newSphere = sphere(3)
+print(newSphere.get_volume())
